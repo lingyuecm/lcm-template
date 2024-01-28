67da934bd2c2ae60dd5cf3fcb818668712de72ad
lingyuecm <132631818+lingyuecm@users.noreply.github.com>
2024-01-28 19:24:49 +0800

    The content should exceed the root window

--- template-react-frontend/src/views/dashboard/Dashboard.js
+++ template-react-frontend/src/views/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './Dashboard.css';
+import {getAccessToken} from "../../utils/cacheManager";
 
 export default class Dashboard extends React.Component {
     render() {
         return (
             <div>
-                <p>I don't know what should be displayed in this page for such a simple RBAC system, but I feel there should be a dashboard</p>
+                <p>I don't know what should be displayed in this page for such a simple RBAC system,
+                    but I feel there should be a dashboard,
+                    so I'd like to display the access token below</p>
+                <p>{ getAccessToken() }</p>
             </div>
         )
     }
